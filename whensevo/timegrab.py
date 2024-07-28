4622da367abdda422710dacf80e60e17dad1b6bd
Ismael <leamsifigs@users.noreply.github.com>
2024-07-28 01:03:54 -0700

    adding an application and some basic modules for the beggining of this project

---
+++ whensevo/timegrab.py
@@ -0,0 +1,17 @@
+import functools
+import datetime
+import pytz
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, url_for
+)
+
+bp = Blueprint("timegrab",  __name__, url_prefix='/time')
+
+@bp.route('/grab')
+def grab():
+    currentDate = datetime.datetime.now(datetime.UTC)
+    evoDate = datetime.datetime(2025, 8, 1, 10, tzinfo = pytz.utc)
+    timeUntilEVO = evoDate - currentDate
+    
+    return render_template('timegrab/date.html' , timeUntilEVO=timeUntilEVO)
